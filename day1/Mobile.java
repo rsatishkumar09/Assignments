149c4f5264b4f8a28d908b56fbba51c093262fa9
rsatishkumar09 <122198002+rsatishkumar09@users.noreply.github.com>
2023-01-08 18:09:07 +1100

    Add files via upload
    Jan 07/Satish/Day1

---
+++ day1/Mobile.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mobNum = 5555;
+		boolean mobBoo = true;
+		char mobType = 'O';
+		String mobModel = "Plus";
+		System.out.println("Number=" + mobNum);
+		System.out.println(mobBoo);
+		System.out.println(mobType);
+		System.out.println(mobModel);
+	}
+
+}
